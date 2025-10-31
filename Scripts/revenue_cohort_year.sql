1efdcc8ad0922a5c5bc71ab24b25378108398d93
Dmitry Kuvyrdin <dmitry.kuvy@gmail.com>
2025-10-31 14:49:45 -0400

    first commit

---
+++ Scripts/revenue_cohort_year.sql
@@ -0,0 +1,8 @@
+SELECT 
+	cohort_year,
+	COUNT(DISTINCT customerkey) AS total_customers,
+	SUM(total_net_revenue) AS total_revenue,
+	SUM(total_net_revenue)/COUNT(DISTINCT customerkey) AS customer_revenue
+FROM cohort_analysis
+GROUP BY
+	cohort_year
